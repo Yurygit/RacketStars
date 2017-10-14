c0a4cb5662fadd5b556d4d782f2abac0f544a598
yurygit <ravil.malay@gmail.com>
2017-10-14 11:57:50 +0800

    intial build 101

--- app/src/main/java/com/bladesappfest/firebase/codelab/racketstars/CodelabPreferences.java
+++ app/src/main/java/com/bladesappfest/firebase/codelab/racketstars/CodelabPreferences.java
@@ -17,6 +17,7 @@ package com.bladesappfest.firebase.codelab.racketstars;
 
 public class CodelabPreferences {
 
+    public int testint=0;
     public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
     public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
 
